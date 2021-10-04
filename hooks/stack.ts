a16fc7428edefd5ce7f8c1ce0e03faa7a4d2448f
t-kusakabe <ttmac0704@gmail.com>
2021-10-04 14:08:42 +0900

    useStateをuseReducerに書き直す

--- hooks/stack.ts
+++ hooks/stack.ts
@@ -1,4 +1,31 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+type StackState<T> = T[];
+
+type StackAction<T> =
+  | { type: 'ACTION_POP' }
+  | { type: 'ACTION_PUSH'; value: T }
+  | { type: 'ACTION_RESET'; initStack: T[] };
+
+const stackReducer = <T>() => (
+  stack: StackState<T>,
+  action: StackAction<T>
+): StackState<T> => {
+  switch(action.type) {
+    case 'ACTION_POP':
+      if (stack.length === 0) {
+        return stack;
+      }
+
+      return [...stack.slice(0, stack.length - 1)];
+
+    case 'ACTION_PUSH':
+      return [...stack, action.value];
+
+    case 'ACTION_RESET':
+      return action.initStack;
+  }
+};
 
 export interface Stack<T> {
   Pop: () => void;
@@ -9,26 +36,11 @@ export interface Stack<T> {
 
 export const useStack = <T>(init?: T[]): [T, Stack<T>] => {
   const initStack: T[] = init ?? [];
-  const [stack, setStack] = useState<T[]>(initStack);
+  const [stack, dispatch] = useReducer(stackReducer<T>(), initStack);
 
-  const Pop = (): void => {
-    if (stack.length === 0) {
-      return;
-    }
-
-    const newStack = [...stack.slice(0, stack.length - 1)];
-    setStack(newStack);
-  };
-
-  const Push = (v: T): void => {
-    const newStack = [...stack, v];
-    setStack(newStack);
-  };
-
-  const Reset = (): void => {
-    setStack(initStack);
-  };
-
+  const Pop = (): void => dispatch({ type: 'ACTION_POP' });
+  const Push = (value: T): void => dispatch({ type: 'ACTION_PUSH', value });
+  const Reset = (): void => dispatch({ type: 'ACTION_RESET', initStack });
   const Length = (): number => stack.length;
 
   return [stack[stack.length - 1], { Pop, Push, Reset, Length }];
